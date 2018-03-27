606b5bd24282faf2a08c55605401e0b2b88667b8
Sean Halupczynski <sean.halup@gmail.com>
2018-03-27 18:04:21 -0400

    Displaying array of letters guessed and not allowing duplicates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,16 @@
-
-// Loading the game in the window
-window.onload = function gameLoad(event) {
-var loadGame = event.onload;
 var firstGame = true;
 var wordBank = ["pirate", "treasure"];
 var lived = 0;
 var hanged = 0;
-var numberOfGuesses = 10;
 var mysteryWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+var numberOfGuesses = mysteryWord.length;
 var dashes = mysteryWord;
+var guessedLetters = [];
+
+
+// Loading the first game in the window
+window.onload = function gameLoad(event) {
+var loadGame = event.onload;
 
     if (firstGame = true) {
 
@@ -26,28 +28,36 @@ var dashes = mysteryWord;
     };
 };
   
-
 window.addEventListener("keypress", function(event) {
 
-    var guessedLetters = [];
     var letterGuess = event.key;
 
-        if (guessedLetters.includes(letterGuess)) {
-            return false;
-        }
-        else if (letterGuess === "a"||"b"||"c"||"d"||"e"||"f"||"g"||"h"||"i"||"j"||"k"||"l"||"m"||"n"||"o"||"p"||"q"||"r"||"s"||"t"||"u"||"v"||"w"||"x"||"y"||"z") {
+        for (i=numberOfGuesses; i>0; i--) { //Added for loop and printing stopped
+            if (guessedLetters.includes(letterGuess)) {
+                return false;
+            }
+            // //Attempt to only include sting
+            // else if (letterGuess !== "a"||"b"||"c"||"d"||"e"||"f"||"g"||"h"||"i"||"j"||"k"||"l"||"m"||"n"||"o"||"p"||"q"||"r"||"s"||"t"||"u"||"v"||"w"||"x"||"y"||"z") {
+           
+            // }
+            else if (letterGuess === "a"||"b"||"c"||"d"||"e"||"f"||"g"||"h"||"i"||"j"||"k"||"l"||"m"||"n"||"o"||"p"||"q"||"r"||"s"||"t"||"u"||"v"||"w"||"x"||"y"||"z") {
            
             // Add letters to guessedLetters array
             guessedLetters.push(letterGuess)
+            }
+            // Display guessed letters
+            document.getElementById("lettersGuessed").innerHTML = guessedLetters; 
             
-        }
-        else {
-            return false;
-        }
-        
-    // Display guessed letters
-    document.getElementById("lettersGuessed").innerHTML = guessedLetters; 
+            //Reduce guess count
+            if (guessedLetters.includes(letterGuess) = false) {
+                numberOfGuesses - 1;
+            };
 
-    console.log(letterGuess);
-    console.log(guessedLetters.length);
-}, false);  
+            console.log(letterGuess);
+            console.log(guessedLetters.length);
+
+        } 
+    }, false
+); 
+
+
