e00f074b35af89ab28c5a55ff071cbac91c59add
Sean Halupczynski <sean.halup@gmail.com>
2018-03-26 21:00:17 -0400

    attempting adding values to array and displaying array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,48 +2,52 @@
 // Loading the game in the window
 window.onload = function gameLoad(event) {
 var loadGame = event.onload;
-var wordBank = ["pirate"];
+var firstGame = true;
+var wordBank = ["pirate", "treasure"];
 var lived = 0;
 var hanged = 0;
 var numberOfGuesses = 10;
 var mysteryWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-var dashes;
+var dashes = mysteryWord;
 
-    if (loadGame = true) {
+    if (firstGame = true) {
 
-        document.getElementById("wordLetters").forEach(mysteryWord.length).innerHTML = "_ "; 
+        document.getElementById("wordLetters").innerHTML = dashes; 
 
         // Display number of guess
         document.getElementById("guessCount").innerHTML = "Number of guesses left: " + numberOfGuesses;
 
         // Display wins
-        document.getElementById("lived").innerHTML = "Times lived: " + live;
+        document.getElementById("lived").innerHTML = "Times lived: " + lived;
 
         // Display losses
-        document.getElementById("hanged").innerHTML = "Times hanged: " + hang;
+        document.getElementById("hanged").innerHTML = "Times hanged: " + hanged;
 
     };
 };
   
 
-// User enters a letter
-document.onkeyup = function userGuess(event) {
-    // Setting the key event to variable letterGuess
-    var letterGuess = event.key;
+window.addEventListener("keypress", function(event) {
+
     var guessedLetters = [];
+    var letterGuess = event.key;
+
+        if (guessedLetters.includes(letterGuess)) {
+            return false;
+        }
+        else if (letterGuess === "a"||"b"||"c"||"d"||"e"||"f"||"g"||"h"||"i"||"j"||"k"||"l"||"m"||"n"||"o"||"p"||"q"||"r"||"s"||"t"||"u"||"v"||"w"||"x"||"y"||"z") {
+           
+            // Add letters to guessedLetters array
+            guessedLetters.push(letterGuess)
+            
+        }
+        else {
+            return false;
+        }
+        
+    // Display guessed letters
+    document.getElementById("lettersGuessed").innerHTML = guessedLetters; 
 
     console.log(letterGuess);
-
-    if (typeof event !== "undefined") {
-
-        letterGuess = event.keyCode;
-    }
-
-    // Add letters to guessedLetters array
-    guessedLetters.push(String.fromCharCode(letterGuess));
-
     console.log(guessedLetters.length);
-
-    // Display guessed letters
-    document.getElementById("lettersGuessed").innerHTML = guessedLetters;    
-};
+}, false);  
