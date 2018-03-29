0b5e0e2931fb2240f955c24af3a1e3ce8fb8976f
Sean Halupczynski <sean.halup@gmail.com>
2018-03-29 13:48:24 -0400

    took a step back and removed the event listener and built out functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,127 @@
-var firstGame = true;
+// var firstGame = true;
 var wordBank = ["pirate", "treasure"];
 var lived = 0;
 var hanged = 0;
 var mysteryWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+var mysteryWordLength = mysteryWord.length;
 var numberOfGuesses = mysteryWord.length;
-var dashes = mysteryWord;
+var wordUnderscore = "";
+var mysteryWordArray = mysteryWord.split("");
 var guessedLetters = [];
+var alphabet = "abcdefghijklmnopqurstuvwxyz";
+var validGuess = alphabet.split("");
+var correctGuess = [];
 
+//Set word to dashes
+function setUnderscore(mysteryWordArray) {
+    var mysteryWordArray = mysteryWord.split("");
 
-// Loading the first game in the window
-window.onload = function gameLoad(event) {
-var loadGame = event.onload;
+    for (i=0; i<mysteryWordArray.length; i++) {
 
-    if (firstGame = true) {
-
-        document.getElementById("wordLetters").innerHTML = dashes; 
-
-        // Display number of guess
-        document.getElementById("guessCount").innerHTML = "Number of guesses left: " + numberOfGuesses;
-
-        // Display wins
-        document.getElementById("lived").innerHTML = "Times lived: " + lived;
-
-        // Display losses
-        document.getElementById("hanged").innerHTML = "Times hanged: " + hanged;
-
-    };
+        var underscore = wordUnderscore + "_ ";
+        document.getElementById("wordLetters").innerHTML = underscores; 
+    }
 };
-  
-window.addEventListener("keypress", function(event) {
+
+// Is the guess valid?
+function guessValid (event) {
+
+    var letterGuess = event.key;
+    var guessIsValid = false;
+
+    if (validGuess.includes(letterGuess)) {
+        guessIsValid = true;
+    }
+};
+
+//Add the guess to an array to show guesses
+function addGuess (event) {
 
     var letterGuess = event.key;
 
-        for (i=numberOfGuesses; i>0; i--) { //Added for loop and printing stopped
-            if (guessedLetters.includes(letterGuess)) {
-                return false;
-            }
-            // //Attempt to only include sting
-            // else if (letterGuess !== "a"||"b"||"c"||"d"||"e"||"f"||"g"||"h"||"i"||"j"||"k"||"l"||"m"||"n"||"o"||"p"||"q"||"r"||"s"||"t"||"u"||"v"||"w"||"x"||"y"||"z") {
-           
-            // }
-            else if (letterGuess === "a"||"b"||"c"||"d"||"e"||"f"||"g"||"h"||"i"||"j"||"k"||"l"||"m"||"n"||"o"||"p"||"q"||"r"||"s"||"t"||"u"||"v"||"w"||"x"||"y"||"z") {
-           
-            // Add letters to guessedLetters array
-            guessedLetters.push(letterGuess)
-            }
-            // Display guessed letters
-            document.getElementById("lettersGuessed").innerHTML = guessedLetters; 
-            
-            //Reduce guess count
-            if (guessedLetters.includes(letterGuess) = false) {
-                numberOfGuesses - 1;
-            };
+    while (numberOfGuesses > 0) {
 
-            console.log(letterGuess);
-            console.log(guessedLetters.length);
+        if (guessValid.guessIsValid = true) {
 
-        } 
-    }, false
-); 
+            guessedLetters.push(letterGuess);
+
+        };
+    };
+};
+
+// Display guessed letters
+function displayGuess (event) {
+
+    var letterGuess = event.key;
+
+    if (guessedLetters.includes(letterGuess)) {
+
+        return false;
+
+    }
+
+    else {
+
+        document.getElementById("lettersGuessed").innerHTML = guessedLetters;
+    }
+};
+
+// Handling a correct guess
+function correctGuess (event) {
+
+    var letterGuess = event.key;
+
+    if (correctGuess.includes(letterGuess)) {
+
+        // Display guess in mystery word
+    };
+};
+
+// Reducing guess count for incorrect guesses
+function guessCount (event) {
+
+    var letterGuess = event.key;
+
+    if (correctGuess.includes(letterGuess)) {
+        
+        return false;
+    }
+
+    else {
+
+        numberOfGuesses = numberOfGuesses - 1;
+    };
+
+    document.getElementById("guessCount").innerHTML = "Number of guesses left: " + numberOfGuesses;
+};
+
+// Win or loss
+function livedOrHanged (event) {
+    
+    var letterGuess = event.key;
+
+    if (correctGuess.length == mysteryWordArray.length && numberOfGuesses > 0) {
+
+        lived = lived + 1;
+        document.getElementById("lived").innerHTML = "Times lived: " + lived;
+        confirm("You lived!!! Want to play again?");
+    }
+
+    else {
+        
+        hanged = hanged + 1;
+        document.getElementById("hanged").innerHTML = "Times hanged" + hanged;
+        confirm("You have been hanged...Do you believe in reincarnation?")
+    };
+};
 
 
+// Display number of guess
+document.getElementById("guessCount").innerHTML = "Number of guesses left: " + numberOfGuesses;
+    
+// Display wins
+document.getElementById("lived").innerHTML = "Times lived: " + lived;
+    
+// Display losses
+document.getElementById("hanged").innerHTML = "Times hanged: " + hanged;
+    
